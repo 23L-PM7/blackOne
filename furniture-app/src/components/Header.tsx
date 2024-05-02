ce8ff5dd0acd0fc5f77aae196f135bdb81939ed3
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-05-02 17:35:33 +0800

    p

--- furniture-app/src/components/Header.tsx
+++ furniture-app/src/components/Header.tsx
@@ -1,37 +1,82 @@
-import Image from 'next/image';
-import AspectRatio from '@mui/joy/AspectRatio';
-import { IoBagOutline } from "react-icons/io5";
+'use client'
 
+import AppBar from '@mui/material/AppBar';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import * as React from 'react';
-import Dropdown from '@mui/joy/Dropdown';
-import IconButton from '@mui/joy/IconButton';
-import Menu from '@mui/joy/Menu';
-import MenuButton from '@mui/joy/MenuButton';
-import MenuItem from '@mui/joy/MenuItem';
-import MoreVert from '@mui/icons-material/MoreVert';
-
+import Box from '@mui/joy/Box';
+import List from '@mui/joy/List';
+import Divider from '@mui/joy/Divider';
+import JoyDrawer from '@mui/joy/Drawer';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
 
 export function Header() {
+    const [isOpenDrawer, setIsOpenDrawer] = React.useState(false);
+
+    const toggleDrawer = () => setIsOpenDrawer(!isOpenDrawer)
     return (
-        <div>
-            <img src="https://plus.unsplash.com/premium_photo-1671070687931-ee34e2b67bfc?q=80&w=3326&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="chair" />
-            <div className="flex items-center w-full p-4 justify-between">
-                <img src="images/logo.png" alt="logo " className='w-6/12' />
-                <IoBagOutline className="w-3/12" />
-                <Dropdown>
-                    <MenuButton
-                        slots={{ root: IconButton }}
-                        slotProps={{ root: { variant: 'outlined', color: 'neutral' } }}
+        <Stack sx={{ flexGrow: 1, position: 'fixed', bottom: 0, left: 0, right: 0, }} className='z-50'>
+            <AppBar position='static' >
+                <Stack direction='row' justifyContent='end'>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2, }}
+                        onClick={toggleDrawer}
                     >
-                        <MoreVert />
-                    </MenuButton>
-                    <Menu>
-                        <MenuItem>Profile</MenuItem>
-                        <MenuItem>My account</MenuItem>
-                        <MenuItem>Logout</MenuItem>
-                    </Menu>
-                </Dropdown>
-            </div>
-        </div>
-    )
+                        <MenuIcon />
+                    </IconButton>
+                </Stack>
+
+            </AppBar>
+            <Drawer open={isOpenDrawer} onToggleDrawer={toggleDrawer} />
+        </Stack>
+    );
+}
+
+
+
+type DrawerProps = {
+    open: boolean;
+    onToggleDrawer: () => void
+}
+
+type Size = 'sm' | 'md' | 'lg';
+
+
+function Drawer(props: DrawerProps) {
+    const { open, onToggleDrawer } = props
+
+
+    return (
+        <Box sx={{}}>
+            <JoyDrawer open={open} onClose={onToggleDrawer}>
+                <Box
+                    onClick={onToggleDrawer}
+                    onKeyDown={onToggleDrawer}
+                >
+                    <List>
+                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+                            <ListItem key={text}>
+                                <ListItemButton>{text}</ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        {['All mail', 'Trash', 'Spam'].map((text) => (
+                            <ListItem key={text}>
+                                <ListItemButton>{text}</ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </JoyDrawer>
+        </Box>
+
+    );
 }
