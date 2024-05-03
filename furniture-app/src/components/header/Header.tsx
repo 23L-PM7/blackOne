6f3a38e7e8aa69ed97fce5ec8a5b401f7c936c72
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-05-03 20:53:51 +0800

    up

--- furniture-app/src/components/header/Header.tsx
+++ furniture-app/src/components/header/Header.tsx
@@ -12,6 +12,7 @@ import JoyDrawer from '@mui/joy/Drawer';
 import ListItem from '@mui/joy/ListItem';
 import ListItemButton from '@mui/joy/ListItemButton';
 import { Bag, Favorite, Remove, Search, UserPro } from '../items/HeaderIcons';
+import { CiCircleRemove } from "react-icons/ci";
 
 
 
@@ -126,25 +127,25 @@ function DrawerMobile(props: BagDrawerProps) {
             src: 'https://plus.unsplash.com/premium_photo-1676638154798-904c8f6282d6?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWluaW1hbHxlbnwwfHwwfHx8MA%3D%3D',
             title: 'Chair',
             price: 1000,
-            icon: <Remove />
+            icon: <CiCircleRemove />
         },
         {
             src: 'https://images.unsplash.com/photo-1527549993586-dff825b37782',
             title: 'Table',
             price: 1000,
-            icon: <Remove />
+            icon: <CiCircleRemove />
         },
         {
             src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
             title: 'Table',
             price: 900,
-            icon: <Remove />
+            icon: <CiCircleRemove />
         },
         {
             src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
             title: 'Sofa',
             price: 900,
-            icon: <Remove />
+            icon: <CiCircleRemove />
         },
         {
             src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
