e0f8280f812023df9e26d16dfe37f352813c6422
ashrv12 <bshirav@gmail.com>
2024-05-03 17:54:42 +0800

    TOAST

--- furniture-app/src/app/admin/page.tsx
+++ furniture-app/src/app/admin/page.tsx
@@ -1,11 +1,29 @@
 import { Header } from "@/components/Header";
+import { Title } from "@/components/adminComponents/title";
+import Button from "@mui/joy/Button";
+import Add from "@mui/icons-material/Add";
+import { quicksand } from "../layout";
 
 export default function Home() {
   return (
     <div className="bg-[#EDECE9]">
-      <div className="h-screen">
-        <h2 className="">Admin</h2>
-        <h1>Dashboard</h1>
+      <div className="h-screen w-screen flex flex-col items-center">
+        <Title />
+        <div className="border-y-[1px] border-[#343434]">
+          <h1>Content</h1>
+        </div>
+        {/* <Button
+          color="warning"
+          disabled={false}
+          //   onClick={function () {}}
+          size="md"
+          variant="soft"
+        /> */}
+        <button
+          className={`bg-[#A18565] text-[#F5F5F5] ${quicksand.className} py-1 px-5 rounded-sm hover:bg-[#F5F5F5] hover:text-[#343434]`}
+        >
+          Add new item
+        </button>
       </div>
     </div>
   );
---
+++ furniture-app/src/components/adminComponents/title.tsx
@@ -0,0 +1,10 @@
+import { quicksand } from "@/app/layout";
+
+export function Title() {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className={`${quicksand.className} text-xl`}>Admin</h2>
+      <h1 className="text-lg">Dashboard</h1>
+    </div>
+  );
+}
