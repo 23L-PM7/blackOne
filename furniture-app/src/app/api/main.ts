40b7edbd5b566e6cd2eb3a199fb2a2aeb2ab266a
ashrv12 <bshirav@gmail.com>
2024-05-06 17:27:38 +0800

    update

---
+++ furniture-app/src/app/api/main.ts
@@ -0,0 +1,26 @@
+import { dbRequest } from "./dbrequest";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+
+  console.log({ id });
+
+  const { documents } = await dbRequest("furniture", "find");
+  return Response.json(documents);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, description } = body;
+
+  console.log({ body });
+
+  const data = await dbRequest("categories", "insertOne", {
+    document: {
+      name: name,
+      description: description,
+    },
+  });
+  return Response.json(data);
+}
