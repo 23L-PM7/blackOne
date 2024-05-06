a8db6cd4341c7ac6413cef7b40e3c477eb306b21
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-05-06 18:14:42 +0800

    er

--- furniture-app/src/components/header/Header.tsx
+++ furniture-app/src/components/header/Header.tsx
@@ -174,12 +174,7 @@ function DrawerMobile(props: BagDrawerProps) {
             price: 900,
             icon: <Remove />
         },
-        {
-            src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
-            title: 'Table',
-            price: 900,
-            icon: <Remove />
-        },
+
 
     ];
 
