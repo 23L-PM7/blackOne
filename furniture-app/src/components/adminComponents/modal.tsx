a395e41df9a510669062c6bbf4f28e505004d6e1
ashrv12 <bshirav@gmail.com>
2024-05-06 19:32:38 +0800

    modal finished

--- furniture-app/src/components/adminComponents/modal.tsx
+++ furniture-app/src/components/adminComponents/modal.tsx
@@ -6,7 +6,19 @@ import { useState } from "react";
 import Input from "@mui/joy/Input";
 import { cinzel, quicksand } from "@/app/theme";
 import { useFurnitures } from "../utility/utils";
-import { Textarea } from "@mui/joy";
+import { Button, SvgIcon, Textarea, styled } from "@mui/joy";
+
+const VisuallyHiddenInput = styled("input")`
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  white-space: nowrap;
+  width: 1px;
+`;
 
 export default function AdminModal() {
   // modal open close
@@ -136,6 +148,59 @@ export default function AdminModal() {
                 placeholder="Price of Product in $"
               />
             </div>
+            <div>
+              <label className={`${quicksand.className}`}>
+                Photo of Product
+              </label>
+              <Input
+                value={price}
+                type="number"
+                onChange={(e) => setPrice(e.target.valueAsNumber)}
+                sx={{
+                  ...quicksand.style,
+                }}
+                placeholder="Price of Product in $"
+              />
+            </div>
+            {/* upload area */}
+            <Button
+              sx={{
+                backgroundColor: "#F5F5F5",
+                color: "#343434",
+                ...quicksand.style,
+              }}
+              component="label"
+              role={undefined}
+              tabIndex={-1}
+              variant="outlined"
+              color="neutral"
+              startDecorator={
+                <SvgIcon>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                    />
+                  </svg>
+                </SvgIcon>
+              }
+            >
+              Upload a Photo
+              <VisuallyHiddenInput type="file" />
+            </Button>
+
+            <button
+              className={`${quicksand.className} bg-[#A18565] text-[#F5F5F5] py-1 px-5 rounded-sm hover:bg-[#F5F5F5] hover:text-[#343434]`}
+            >
+              Create
+            </button>
           </div>
         </Sheet>
       </Modal>
