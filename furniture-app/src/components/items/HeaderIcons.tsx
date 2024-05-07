d6e8a7053c06cd720b65f3f0d51b7c08c7de4860
ashrv12 <bshirav@gmail.com>
2024-05-07 16:19:35 +0800

    fixed search icon bug

--- furniture-app/src/components/items/HeaderIcons.tsx
+++ furniture-app/src/components/items/HeaderIcons.tsx
@@ -1,61 +1,131 @@
 export function Search() {
-    return (
-        <svg width="30" height="30" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clipPath="url(#clip0_31_139)">
-                <path d="M25.714 49.2857C38.7322 49.2857 49.2854 38.7324 49.2854 25.7143C49.2854 12.6961 38.7322 2.14282 25.714 2.14282C12.6958 2.14282 2.14258 12.6961 2.14258 25.7143C2.14258 38.7324 12.6958 49.2857 25.714 49.2857Z" stroke="#343434" stroke-width="4" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M57.8564 57.8572L42.8564 42.8572" stroke="#343434" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
-            </g>
-            <defs>
-                <clipPath id="clip0_31_139">
-                    <rect width="60" height="60" fill="white" />
-                </clipPath>
-            </defs>
-        </svg>
-    )
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_31_139)">
+        <path
+          d="M25.714 49.2857C38.7322 49.2857 49.2854 38.7324 49.2854 25.7143C49.2854 12.6961 38.7322 2.14282 25.714 2.14282C12.6958 2.14282 2.14258 12.6961 2.14258 25.7143C2.14258 38.7324 12.6958 49.2857 25.714 49.2857Z"
+          stroke="#343434"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M57.8564 57.8572L42.8564 42.8572"
+          stroke="#343434"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_31_139">
+          <rect width="60" height="60" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
 }
 
 export function UserPro() {
-    return (
-        <svg width="40" height="40" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="45" cy="30" r="13"
-                stroke="#343434"
-                strokeWidth="4"
-                strokeLinecap="round" />
-            <path d="M22.2615 53.8298C23.4824 51.7284 25.7897 50.625 28.22 50.625H61.78C64.2103 50.625 66.5176 51.7284 67.7385 53.8298C69.8691 57.4971 72.8766 64.0513 73.1105 72.1235C73.1265 72.6755 72.6773 73.125 72.125 73.125H17.875C17.3227 73.125 16.8735 72.6755 16.8895 72.1235C17.1234 64.0513 20.1309 57.4971 22.2615 53.8298Z"
-                stroke="#343434"
-                strokeWidth="4"
-                strokeLinecap="round" />
-        </svg>
-
-    )
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 90 90"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="45"
+        cy="30"
+        r="13"
+        stroke="#343434"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+      <path
+        d="M22.2615 53.8298C23.4824 51.7284 25.7897 50.625 28.22 50.625H61.78C64.2103 50.625 66.5176 51.7284 67.7385 53.8298C69.8691 57.4971 72.8766 64.0513 73.1105 72.1235C73.1265 72.6755 72.6773 73.125 72.125 73.125H17.875C17.3227 73.125 16.8735 72.6755 16.8895 72.1235C17.1234 64.0513 20.1309 57.4971 22.2615 53.8298Z"
+        stroke="#343434"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
 }
 
-
 export function Favorite() {
-    return (
-        <svg width="30" height="30" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M30.0176 53.0684L6.55605 31.8171C-6.19485 19.0662 12.5489 -5.41545 30.0176 14.3909C47.4862 -5.41545 66.1453 19.1512 53.4793 31.8171L30.0176 53.0684Z" stroke="#343434" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-
-    )
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M30.0176 53.0684L6.55605 31.8171C-6.19485 19.0662 12.5489 -5.41545 30.0176 14.3909C47.4862 -5.41545 66.1453 19.1512 53.4793 31.8171L30.0176 53.0684Z"
+        stroke="#343434"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 }
 
 export function Bag() {
-    return (
-        <svg width="40" height="40" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M20 30L20 20C20 14.4772 24.4772 10 30 10V10C35.5228 10 40 14.4772 40 20L40 30" stroke="#343434" strokeWidth="3" strokeLinecap="round" />
-            <path d="M9.69435 26.1678C9.83942 24.4269 9.91196 23.5565 10.486 23.0282C11.0601 22.5 11.9336 22.5 13.6805 22.5H46.3195C48.0664 22.5 48.9399 22.5 49.514 23.0282C50.088 23.5565 50.1606 24.4269 50.3057 26.1678L52.3195 50.3339C52.404 51.3474 52.4462 51.8542 52.1491 52.1771C51.852 52.5 51.3435 52.5 50.3264 52.5H9.6736C8.65655 52.5 8.14802 52.5 7.85092 52.1771C7.55382 51.8542 7.59605 51.3474 7.68051 50.3339L9.69435 26.1678Z" stroke="#343434" strokeWidth="3" />
-        </svg>
-
-    );
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20 30L20 20C20 14.4772 24.4772 10 30 10V10C35.5228 10 40 14.4772 40 20L40 30"
+        stroke="#343434"
+        strokeWidth="3"
+        strokeLinecap="round"
+      />
+      <path
+        d="M9.69435 26.1678C9.83942 24.4269 9.91196 23.5565 10.486 23.0282C11.0601 22.5 11.9336 22.5 13.6805 22.5H46.3195C48.0664 22.5 48.9399 22.5 49.514 23.0282C50.088 23.5565 50.1606 24.4269 50.3057 26.1678L52.3195 50.3339C52.404 51.3474 52.4462 51.8542 52.1491 52.1771C51.852 52.5 51.3435 52.5 50.3264 52.5H9.6736C8.65655 52.5 8.14802 52.5 7.85092 52.1771C7.55382 51.8542 7.59605 51.3474 7.68051 50.3339L9.69435 26.1678Z"
+        stroke="#343434"
+        strokeWidth="3"
+      />
+    </svg>
+  );
 }
 
 export function Remove() {
-    return (
-        <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M42 2L2 42" stroke="#343434" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M2 2L42 42" stroke="#343434" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-
-    )
+  return (
+    <svg
+      width="44"
+      height="44"
+      viewBox="0 0 44 44"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M42 2L2 42"
+        stroke="#343434"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2 2L42 42"
+        stroke="#343434"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 }
