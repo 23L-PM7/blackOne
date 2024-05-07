6b28ef21f15b50c668a674155eb0e80a91dce2d6
ashrv12 <bshirav@gmail.com>
2024-05-07 16:25:55 +0800

    Cloudinary env

--- furniture-app/next.config.mjs
+++ furniture-app/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  },
+};
 
 export default nextConfig;
