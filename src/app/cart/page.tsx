f1dbea472a918759d66dff9bc8b42276aacccbbb
ashrv12 <bshirav@gmail.com>
2024-05-08 20:49:16 +0800

    cart update

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { CartItem } from "@/components/cartComponents/item";
 import { cinzel, quicksand } from "../theme";
 
 export default function Home() {
@@ -9,15 +10,8 @@ export default function Home() {
           <h1 className={`${quicksand.className} text-[50px] h-[50px]`}>MY</h1>
           <h2 className={`${cinzel.className} text-[50px] h-[54px]`}>CART</h2>
         </div>
-        <div className="w-[90%] h-[50px] border-b-[1px] border-[#343434] mt-[40px]">
-          <div className="w-full flex justify-between">
-            <h1 className={`${quicksand.className} font-medium`}>
-              PRODUCT<span className="font-bold"> : </span>
-            </h1>
-            <h1 className={`${cinzel.className} font-medium text-xl`}>
-              PRODUCT NAME
-            </h1>
-          </div>
+        <div className="w-[90%]">
+          <CartItem />
         </div>
       </div>
     </div>
