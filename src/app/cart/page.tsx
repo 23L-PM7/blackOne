3d1bd1d54c88b80341f8b649d42ef253bd44db9a
ashrv12 <bshirav@gmail.com>
2024-05-09 16:36:34 +0800

    cart order

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { CartItem } from "@/components/cartComponents/item";
 import { cinzel, quicksand } from "../theme";
+import { CartTotals } from "@/components/cartComponents/cartTotals";
 
 export default function Home() {
   return (
@@ -13,12 +14,7 @@ export default function Home() {
         <div className="w-[90%]">
           <CartItem />
         </div>
-        <div className="w-[90%] flex flex-col items-center">
-          <h1 className={`${cinzel.className} text-[25px]`}>CART TOTALS</h1>
-          <div className="w-[90%]">
-            <h1 className="text-[20px]">Subtotal : </h1>
-          </div>
-        </div>
+        <CartTotals />
       </div>
     </div>
   );
