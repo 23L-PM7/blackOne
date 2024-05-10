ed8637b7f08449f25657436d7679c78af140e815
ashrv12 <bshirav@gmail.com>
2024-05-10 18:25:26 +0800

    finished single

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { useFurnitures } from "@/components/utility/utils";
 
 export default function Home() {
   // retrieve furnitures
+  // but the single comes in an array so we need to add .[0]
   const { single, loadSingleFurniture }: any = useFurnitures();
 
   // retrieve the params
