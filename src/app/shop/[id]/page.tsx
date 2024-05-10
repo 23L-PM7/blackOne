5c164bfc20e567c5f01ca842ea6e35197cf8f0f7
ashrv12 <bshirav@gmail.com>
2024-05-10 16:54:43 +0800

    update

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+import { useFurnitures } from "@/components/utility/utils";
+
 export default function Home() {
+  // retrieve furnitures
+  const { furnitures }: any = useFurnitures();
+
+  // retrieve the params
+  const params = useParams<{ id: string }>();
+
+  //   decode the param %20 to white space
+  const decodedname = decodeURI(params.id);
+
+  //   find the right product
+  const product = furnitures.filter((item: any) => item.name === decodedname);
+
+  // console log check
+  console.log(product);
+
   return (
     <div>
-      <div></div>
+      <div>
+        <h1>{decodedname}</h1>
+      </div>
     </div>
   );
 }
