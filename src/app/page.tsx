a46896493ed139f4d24073df2957a8f8adc44133
ashrv12 <bshirav@gmail.com>
2024-05-11 17:11:41 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-import { Header } from "@/components/header/Header";
+"use client";
 
+import { Header } from "@/components/header/Header";
+import { Loader } from "@/components/loader";
+import { useFurnitures } from "@/components/utility/utils";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { furnitures, loadFurnitures }: any = useFurnitures();
+
+  useEffect(() => {
+    loadFurnitures();
+  }, []);
+
+  if (furnitures.length === 0) {
+    return <Loader />;
+  }
+
   return (
     <div>
       <Header />
