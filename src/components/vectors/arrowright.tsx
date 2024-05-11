19f8e0d91ccfe2b63fe28641f737f6f6ee8a97a0
ashrv12 <bshirav@gmail.com>
2024-05-11 19:07:53 +0800

    home home is finished

---
+++ src/components/vectors/arrowright.tsx
@@ -0,0 +1,14 @@
+export function ArrowRight() {
+  return (
+    <svg
+      width="65"
+      height="65"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M12 18L18 12L12 6" stroke="#A18565" strokeWidth="1" />
+      <path d="M6 18L12 12L6 6" stroke="#A18565" strokeWidth="1" />
+    </svg>
+  );
+}
