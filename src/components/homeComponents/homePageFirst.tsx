502d0f2f774f625a68ad8ca66ade1145f80f9f32
Yesui <164152047+yesuia09@users.noreply.github.com>
2024-05-13 18:36:55 +0800

    asdf

--- src/components/homeComponents/homePageFirst.tsx
+++ src/components/homeComponents/homePageFirst.tsx
@@ -5,7 +5,7 @@ import { ArrowRight } from "../vectors/arrowright";
 export function FirstItem() {
   return (
     <div className="w-full h-full snap-start overflow-hidden flex-shrink-0 relative md:flex">
-      <div className="hidden md:block">
+      <div className="hidden md:block w-[50%]">
         <img
           src="https://www.karageorgiou.gr/wp-content/uploads/2022/11/TM4A8259.jpg"
           className="object-cover w-full h-auto"
@@ -24,7 +24,9 @@ export function FirstItem() {
         </div>
 
         <div className="w-[80%] flex justify-end">
-          <h1 className={`${cinzel.className} text-[37px] h-[37px]`}>
+          <h1
+            className={`${cinzel.className} md: text-center text-[37px] h-[37px]`}
+          >
             For Quality
           </h1>
         </div>
@@ -32,19 +34,19 @@ export function FirstItem() {
       {/* desktop title */}
       <div className="hidden w-[50%] h-full md:flex flex-col items-center pt-[150px] gap-10">
         <div className="w-[80%]">
-          <div className="w-[60%] flex">
+          <div className="w-[70%] lg:w-[60%] flex">
             <h1 className={`${cinzel.className} text-[40px] h-[40px]`}>
               Passion
             </h1>
           </div>
-          <div className="w-[50%] flex justify-end">
+          <div className="w-[100%] sm:w-[70%] lg:w-[64%] xl:w-[51%] min-[1757px]:w-[40%] flex justify-end ">
             <h1 className={`${cinzel.className} text-[40px] h-[40px]`}>
               For Quality
             </h1>
           </div>
         </div>
 
-        <p className="w-[80%] pt-[70px] text-[20px]">
+        <p className="w-[80%] pt-[20px] text-[20px]">
           For three generations and nearly a century, Karageorgiou Interiors has
           been delivering elegant, timeless spaces and made-to-measure, hand
           crafted, fine furniture that excite the eye and nurture the soul.{" "}
