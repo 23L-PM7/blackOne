351367393675d5e63dad94eb80a72a29f075e4f8
ashrv12 <bshirav@gmail.com>
2024-05-14 16:31:50 +0800

    update

--- src/components/homeComponents/homePageFirst.tsx
+++ src/components/homeComponents/homePageFirst.tsx
@@ -5,15 +5,15 @@ import { ArrowRight } from "../vectors/arrowright";
 export function FirstItem() {
   return (
     <div className="w-full h-full snap-start overflow-hidden flex-shrink-0 relative md:flex">
-      <div className="hidden md:block w-[50%]">
+      <div className="hidden w-full h-full md:block w-[50%]">
         <img
           src="https://www.karageorgiou.gr/wp-content/uploads/2022/11/TM4A8259.jpg"
-          className="object-cover w-full h-auto"
+          className="object-cover w-full h-full"
         />
       </div>
       <img
         src="https://www.karageorgiou.gr/wp-content/uploads/2022/11/TM4A8259.jpg"
-        className="object-cover h-full w-auto md:hidden"
+        className="object-cover h-full w-full md:hidden"
       />
       {/* mobile title */}
       <div className="md:hidden absolute top-0 flex flex-col p-4 w-full h-[23%] justify-center">
