99b3074bd3fc024b385426d547f2039d23254911
ashrv12 <bshirav@gmail.com>
2024-05-15 17:20:19 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,19 +3,40 @@ import { CssVarsProvider } from "@mui/joy";
 import "./globals.css";
 import customTheme, { quicksand } from "./theme";
 import { Header } from "@/components/header/Header";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import Footer from "@/components/footer/Footer";
+import { useRef } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const containerRef = useRef(null);
+
   return (
     <html lang="en">
       {/* <CssVarsProvider theme={customTheme}> */}
       <body className={`${quicksand.className} text-[#343434]`}>
-        <Header />
-        {children}
+        <LocomotiveScrollProvider
+          options={{
+            smooth: true,
+            // ... all available Locomotive Scroll instance options
+          }}
+          watch={
+            [
+              //..all the dependencies you want to watch to update the scroll.
+              //  Basicaly, you would want to watch page/location changes
+              //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+            ]
+          }
+          containerRef={containerRef}
+        >
+          <main data-scroll-container ref={containerRef}>
+            <Header />
+            {children}
+          </main>
+        </LocomotiveScrollProvider>
       </body>
       {/* </CssVarsProvider> */}
     </html>
