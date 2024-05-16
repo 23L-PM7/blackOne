ff7a0513f6c24fc2f1baf70d42eee30cc482ac2a
MNDE25 <151522647+MNDE25@users.noreply.github.com>
2024-05-16 18:01:29 +0800

    m

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,10 @@
 import Footer from "@/components/footer/Footer";
+import { Header } from "@/components/header/Header";
 
 export default function Shop() {
   return (
     <div>
+      <Header />
       <Footer />
     </div>
   );
