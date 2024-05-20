6850719418318ada9fc7d3aa924d56a00cd29aeb
MNDE25 <151522647+MNDE25@users.noreply.github.com>
2024-05-20 19:02:22 +0800

    m

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,7 +3,6 @@
 import Footer from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
 import { Searching } from "@/components/seaching/searching";
-import { Shopping } from "@/components/shop/Shop";
 import { useSearchParams } from "next/navigation";
 
 export default function Home() {
