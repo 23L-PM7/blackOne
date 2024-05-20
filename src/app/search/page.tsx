f3a4bb8ba9a44d76bceda655a1a6d513d6058450
MNDE25 <151522647+MNDE25@users.noreply.github.com>
2024-05-20 16:55:15 +0800

    uytres

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Footer from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+
   return (
     <div>
       <Header />
+      <div className="h-96 pt-52">Search, {query}</div>
       <Footer />
     </div>
   );
