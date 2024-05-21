9e71197c6d4dd6873fc9b71400d62ec7732cba61
ashrv12 <bshirav@gmail.com>
2024-05-21 18:47:53 +0800

    search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
 import { Searching } from "@/components/seaching/searching";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Home() {
   const searchParams = useSearchParams();
@@ -12,7 +13,10 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <Searching />
+      <Suspense>
+        <Searching />
+      </Suspense>
+
       <div className="h-96 pt-52">Search, {query}</div>
       <Footer />
     </div>
